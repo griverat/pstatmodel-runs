b273fa0b2795d897ea7bea7497099d863023343b
Gerardo Rivera Tello <gerardo_art@me.com>
2021-05-20 23:35:24 -0500

    Reset index after query to maintain alignment

--- RUNS/MAY/run_model.py
+++ RUNS/MAY/run_model.py
@@ -129,8 +129,7 @@ for val_year in range(1981, 2017):
     months_val_index = pisco_val.groupby("time.month").groups
 
     # Removing val_year - 1 from predictors data
-    sel_db_val = sel_db.query("year!=@val_year-1").copy()
-    sel_db_val.loc[:, "const"] = 1
+    sel_db_val = sel_db.query("year!=@val_year-1").reset_index(drop=True)
 
     for mnum, mmodel in full_model.items():
         print(f"Computing validating month number {mnum}", flush=True)
