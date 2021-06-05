cee72329f76be2a754130ca16960696af45c80da
Gerardo Rivera Tello <gerardo_art@me.com>
2021-06-04 20:23:45 -0500

    Add shell script to simplify runs

---
+++ run_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -i
+
+source ~/miniconda3/etc/profile.d/conda.sh
+
+conda activate pangeo
+
+MONTH_DIR="/home/grivera/GitLab/pstatmodel-runs/RUNS/MAY"
+SETTINGS_PATH="$MONTH_DIR/settings.json"
+
+BASE_DIR=$(pwd)
+
+cd "$MONTH_DIR"
+python create_database.py $SETTINGS_PATH
+
+cd "$BASE_DIR/src"
+sbatch --export=SETTINGS_PATH --output=$MONTH_DIR/modelrun-log.txt run_model.sbatch
