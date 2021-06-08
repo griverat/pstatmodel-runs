c08773de0624f8b35a50fe2742d449e0f1fc2347
Gerardo Rivera Tello <gerardo_art@me.com>
2021-06-07 21:01:23 -0500

    Add settings for AUG run

---
+++ RUNS/AUG/create_database.py
@@ -0,0 +1,20 @@
+#%%
+import pandas as pd
+import pstatmodel as psm
+import pstatmodel.utils as psu
+
+#%%
+ModelPredictor = psm.ModelVariables()
+#%%
+ecData = pd.read_csv("ec_ersstv5.txt", parse_dates=[0])
+ModelPredictor.register_variable("EC_index", ["E", "C"], ecData)
+
+#%%
+shift_kwargs = dict(init_month="08", fyear=2022)
+ModelPredictor.shiftAllVariables(**shift_kwargs)
+
+model_init_data = ModelPredictor.get_datatable()
+
+# %%
+model_init_data.dropna(axis=1, how="all").to_excel("Predictores_IniAug.xlsx")
+# %%
