6c9b6491fd12f716a18572b2c94670a8058d6765
Gerardo Rivera Tello <gerardo_art@me.com>
2021-07-07 00:28:43 -0500

    Add parametrized plot notebook for the diagnostic run

--- run_model.sh
+++ run_model.sh
@@ -18,3 +18,4 @@ sbatch --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$MONTH_DIR/modelrun-lo
 
 cd "$BASE_DIR"
 papermill notebooks/plotting_template.ipynb $MONTH_DIR/plotting_$MONTH.ipynb -p settings $SETTINGS_PATH
+papermill notebooks/plot_diagnostic_template.ipynb $MONTH_DIR/plot_diagnostic_$MONTH.ipynb -p settings $SETTINGS_PATH
