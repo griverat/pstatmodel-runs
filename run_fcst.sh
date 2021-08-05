c6f14ca59967d5b1e4c009a2e6ba85d928006fd8
Gerardo Rivera Tello <gerardo_art@me.com>
2021-08-05 11:39:33 -0500

    Wait for job to finish before calling papermill

--- run_fcst.sh
+++ run_fcst.sh
@@ -4,7 +4,7 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate pangeo
 
-MONTH="JUN"
+MONTH="JUL"
 MONTH_DIR="/home/grivera/GitLab/pstatmodel-runs/RUNS/$MONTH"
 SETTINGS_PATH="$MONTH_DIR/settings.json"
 
@@ -14,7 +14,9 @@ cd "$MONTH_DIR"
 python create_database.py $SETTINGS_PATH
 
 cd "$BASE_DIR/src"
-sbatch --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$MONTH_DIR/fcstrun-log.txt --job-name="fcst$MONTH" run_fcst.sbatch
+sbatch -W --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$MONTH_DIR/fcstrun-log.txt --job-name="fcst$MONTH" run_fcst.sbatch
+
+wait
 
 cd "$BASE_DIR"
 papermill notebooks/fcst_plot_template.ipynb $MONTH_DIR/fcst_plot_$MONTH.ipynb -p settings $SETTINGS_PATH
--- run_model.sh
+++ run_model.sh
@@ -4,7 +4,7 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate pangeo
 
-MONTH="OCT"
+MONTH="JUL"
 MONTH_DIR="/home/grivera/GitLab/pstatmodel-runs/RUNS/$MONTH"
 SETTINGS_PATH="$MONTH_DIR/settings.json"
 
@@ -14,7 +14,9 @@ cd "$MONTH_DIR"
 python create_database.py $SETTINGS_PATH
 
 cd "$BASE_DIR/src"
-sbatch --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$MONTH_DIR/modelrun-log.txt --job-name="model$MONTH" run_model.sbatch
+sbatch -W --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$MONTH_DIR/modelrun-log.txt --job-name="model$MONTH" run_model.sbatch
+
+wait
 
 cd "$BASE_DIR"
 papermill notebooks/plotting_template.ipynb $MONTH_DIR/plotting_$MONTH.ipynb -p settings $SETTINGS_PATH
