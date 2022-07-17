bff1180a25452fc1ad39f81eff2a3dbf9edb06f2
Gerardo Rivera Tello <gerardo_art@me.com>
2022-07-17 13:08:30 -0500

    Create dir to store logs

--- run_model.sh
+++ run_model.sh
@@ -4,17 +4,20 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate pstatmodel
 
-DATE="2021/05"
+DATE="2022/07"
 MONTH=$(date -d $DATE/01 +%Y.%m)
 MONTH_PATH=$(date -d $DATE/01 +%Y/%m.%^b)
 MONTH_DIR="/home/grivera/GitLab/pstatmodel-runs/RUNS/$MONTH_PATH"
+LOGS="$MONTH_DIR/logs"
 SETTINGS_PATH="$MONTH_DIR/settings.json"
+
+mkdir -p $LOGS
 
 BASE_DIR=$(pwd)
 
 cd "$BASE_DIR/src"
 python create_database.py $SETTINGS_PATH
-sbatch -W --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$MONTH_DIR/modelrun-log_$(date +%Y-%m-%d_%H:%M).txt --job-name="p$MONTH" run_model.sbatch
+sbatch -W --export=ALL,SETTINGS_PATH=$SETTINGS_PATH --output=$LOGS/modelrun-log_$(date +%Y-%m-%d_%H:%M).txt --job-name="p$MONTH" run_model.sbatch
 
 wait
 
